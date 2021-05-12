61e8263ca1509495da03d3af7d4193ff6be6b5b0
Lennart Keller <lennart.keller@gmail.com>
2021-05-12 22:31:12 +0200

    add first draft of task management

---
+++ src/views/SequenceClassificationTask.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="column is-vcentered">
+    <section class="column">
+      <b-field position="is-centered">
+            <b-radio-button v-for="cls in classes" v-bind:key="cls" v-model="selectedClass" :native-value="cls"
+                type="is-primary is-light is-outlined">
+                <b-icon v-if="selectedClass === cls" icon="check"></b-icon>
+                <span>{{cls}}</span>
+            </b-radio-button>
+      </b-field>
+    </section>
+    <p>{{selectedClass}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SequenceClassificationTask',
+  data () {
+    return {
+      classes: ['1', '2'],
+      selectedClass: null
+    }
+  },
+  computed: {
+    finished: function () {
+      return this.selectedClass !== null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,23 +1,29 @@
 <template>
 <div id="task-view" class="container">
-  <TextBox v-bind:textId="currentTextId" :key="currentTextId"></TextBox>
+  <TextBox v-bind:textId="currentTextId" :key="currentTextId" ref="textBox"></TextBox>
+  <SequenceClassificationTask :key="currentTextId + 'task'" ref="task-0"/> <!-- Hacky-->
+  <SequenceClassificationTask :key="currentTextId + 'task1'" ref="task-1"/>
   <br>
-  <b-button class="button-text-control" id="button-previous" type="is-primary is-light" v-on:click="decrementCurrentId">Prev</b-button>
-  <b-button class="button-text-control" id="button-next" type="is-primary is-light" v-on:click="incrementCurrentId">Next</b-button>
+  <b-message v-if="error !== null" type="is-danger" has-icon>{{error}}</b-message>
+  <b-button class="button-text-control" id="button-previous" type="is-primary is-light" v-on:click="onClickPrevious">Prev</b-button>
+  <b-button class="button-text-control" id="button-next" type="is-primary is-light" v-on:click="onClickNext">Next</b-button>
+  <p v-on:click="onClickPrevious">Test</p>
 </div>
 </template>
 
 <script>
 // import $backend from '../backend'
 import TextBox from '@/views/TextBox'
+import SequenceClassificationTask from '@/views/SequenceClassificationTask'
 
 export default {
   name: 'TaskView',
-  components: {TextBox},
+  components: {SequenceClassificationTask, TextBox},
   data () {
     return {
       collectionData: {},
-      currentTextId: 1
+      currentTextId: 1,
+      error: null
     }
   },
   methods: {
@@ -26,6 +32,35 @@ export default {
     },
     decrementCurrentId () {
       this.currentTextId--
+    },
+    checkTaskStates () {
+      this.error = null
+      if (!this.allTasksFinished) {
+        this.error = 'Not all Tasks are finished.'
+        return false
+      }
+      return true
+    },
+    onClickPrevious () {
+      if (this.checkTaskStates()) {
+        this.decrementCurrentId()
+      }
+    },
+    onClickNext () {
+      if (this.checkTaskStates()) {
+        this.incrementCurrentId()
+      }
+    }
+  },
+  computed: {
+    allTasksFinished: function () {
+      let states = []
+      for (let key in this.$refs) {
+        if (key.startsWith('task-')) {
+          states.push(this.$refs[key].finished)
+        }
+      }
+      return states.every(elem => elem)
     }
   }
 }
