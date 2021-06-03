6c1c0becb062802eecfd10820dcd1d3b14135cfd
Lennart Keller <lennart.keller@googlemail.com>
2021-06-03 14:45:01 +0200

    refactor app context handling using decorators

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-
+from flask.cli import with_appcontext
 import click
 
 from app import app, db
@@ -20,13 +20,12 @@ def db_handling():
 
 
 @db_handling.command()
+@with_appcontext
 def init_db():
     db_path = Path('./app/db')
     if not db_path.exists():
         db_path.mkdir()
-
-    with app.app_context():
-        db.create_all()
+    db.create_all()
 
 
 @db_handling.command()
@@ -45,39 +44,40 @@ def create_collection():
 
 @create_collection.command()
 @click.argument('filename', type=click.File('r'))
+@with_appcontext
 def from_json(filename):
-    with app.app_context():
-        collection_src = json.load(filename)
+
+    collection_src = json.load(filename)
+    try:
+        collection_config = collection_src['Config']
+    except KeyError:
+        click.echo("Invalid collection src. Missing config section!")
+        return
+
+    try:
+        collection_data = collection_src['Texts']
+    except KeyError:
+        click.echo("Invalid collection src. Missing data section!")
+        return
+
+    collection_id = handle_collection_config(collection_config)
+
+    if isinstance(collection_data, list):
         try:
-            collection_config = collection_src['Config']
-        except KeyError:
-            click.echo("Invalid collection src. Missing config section!")
+            texts = create_texts_from_list(collection_data, collection_id)
+        except Exception as e:
+            click.echo(e)
+            return
+    elif isinstance(collection_data, dict):
+        try:
+            texts = create_texts_from_read_config(collection_data, collection_id)
+        except Exception as e:
+            click.echo(e)
             return
 
-        try:
-            collection_data = collection_src['Texts']
-        except KeyError:
-            click.echo("Invalid collection src. Missing data section!")
-            return
-
-        collection_id = handle_collection_config(collection_config)
-
-        if isinstance(collection_data, list):
-            try:
-                texts = create_texts_from_list(collection_data, collection_id)
-            except Exception as e:
-                click.echo(e)
-                return
-        elif isinstance(collection_data, dict):
-            try:
-                texts = create_texts_from_read_config(collection_data, collection_id)
-            except Exception as e:
-                click.echo(e)
-                return
-
-        # Finally commit all changes to the db
-        db.session.commit()
-        click.echo(f"Imported collection {collection_config['Name']} with {len(texts)} into the application.")
+    # Finally commit all changes to the db
+    db.session.commit()
+    click.echo(f"Imported collection {collection_config['Name']} with {len(texts)} into the application.")
 
 
 @click.group()
@@ -89,16 +89,17 @@ def write_collection():
 @click.option('--name', '-n', required=True)
 @click.option('--only-annotated-texts', is_flag=True)
 @click.argument('filename', type=click.File('w'))
+@with_appcontext
 def to_json(name, filename, only_annotated_texts):
-    with app.app_context():
-        collection_query = list(Collection.query.filter_by(name=name))
-        if not collection_query:
-            click.echo(f"No collection with name {name} in database")
-            return
-        collection = collection_query[0]
-        data = collection_to_dict(collection, only_annotated_texts=only_annotated_texts)
-        json.dump(data, filename)
-        click.echo(f'Created output file {filename.name}')
+
+    collection_query = list(Collection.query.filter_by(name=name))
+    if not collection_query:
+        click.echo(f"No collection with name {name} in database")
+        return
+    collection = collection_query[0]
+    data = collection_to_dict(collection, only_annotated_texts=only_annotated_texts)
+    json.dump(data, filename)
+    click.echo(f'Created output file {filename.name}')
 
 
 cli = click.CommandCollection(sources=[db_handling, create_collection, write_collection])
